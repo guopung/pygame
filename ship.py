ccadccfe531fec2e60ba6a274e64a3f4d0e0756a
ping <guopung@163.com>
2022-02-16 16:54:53 +0800

    first commit

---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+ 
+class Ship:
+    """A class to manage the ship."""
+ 
+    def __init__(self, ai_settings, screen):
+        """Initialize the ship and set its starting position."""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.ship_speed_factor = 1.5
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        # 根据self.center更新rect对象
+        self.rect.centerx = self.center    
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
